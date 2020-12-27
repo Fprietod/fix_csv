45a9167f4996a16251115c7276427679d15e0dc2
F.Prieto <felipe00@live.com.mx>
2020-12-26 23:21:51 -0600

    Update clean_csv.py

--- clean_csv.py
+++ clean_csv.py
@@ -1,19 +1,13 @@
 import pandas as pd
 import csv
 
-#Para el delimitador utilizamos funciones que contiene la librería de csv
-with open('starts_data.csv', 'r') as infile, open('output.psv', 'w') as outfile:
-    reader = csv.reader(infile)
-    writer = csv.writer(outfile, delimiter='|')
-    writer.writerows(reader)
-    
-#Es necesario convertirlo en un data frame nunevo para poder leerlo
+# Lo que vamos a hacer es acomodar las columnas para que cada línea contenga el mismo número de columnas en este caso el alrchivo se encuentra bastante #dañado así que renombraremos las columnas de una manera manual 
 rows = []
-csv_header = ['user', 'item', 'time', 'rating', 'review']
+csv_header = ['id', 'item', 'time', 'rating', 'review']
 frame_header = ['user', 'item', 'rating', 'review']
 
-
-# Conversión
+# Con DictReader nosotros vamos a poder acceder a la cabecera, lo que se hará es recorrer nuestros campos, si hay un dato que falta
+#Insertaremos un dato de la cabecera para que así cada línea contenga el mismo número de columnas y se pueda leer el archivo
 with open('starts_data.csv', 'r') as f_input:
     for row in csv.DictReader(f_input, delimiter=' ', fieldnames=csv_header[:-1], restkey=csv_header[-1], skipinitialspace=True):
         try:
@@ -23,6 +17,13 @@ with open('starts_data.csv', 'r') as f_input:
 frame = pd.DataFrame(rows, columns=frame_header)
 print (frame)
 
-#Convertimos nuestro DataFrame en un CSV
 
-frame.to_csv('outpout.csv', encoding='utf-8')
+#Nuestro data Frame lo vamos a convertir en un CSV para que pueda ser leido con un encoding de utf-8
+frame.to_csv('outpout.csv', encoding='utf-8') 
+
+# Crearemos el archivo psv con las comillas faltantes
+#Para el delimitador utilizamos funciones que contiene la librería de csv
+with open('starts_data.csv', 'r') as infile, open('output.psv', 'w') as outfile:
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile, delimiter='|')
+    writer.writerows(reader)
