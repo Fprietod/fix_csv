01393a97afbda2c489b9306b8de8d332517bcd2d
F.Prieto <felipe00@live.com.mx>
2020-12-27 02:14:56 -0600

    Update clean_csv.py

--- clean_csv.py
+++ clean_csv.py
@@ -1,29 +1,38 @@
 import pandas as pd
 import csv
 
-# Lo que vamos a hacer es acomodar las columnas para que cada línea contenga el mismo número de columnas en este caso el alrchivo se encuentra bastante #dañado así que renombraremos las columnas de una manera manual 
+# Lo que vamos a hacer es acomodar las columnas para que cada línea contenga el mismo número de columnas en este caso el alrchivo se encuentra dañado
+# Por lo que se asume algunas cabeceras, para probar el archivo
 rows = []
-csv_header = ['id', 'item', 'time', 'rating', 'review']
-frame_header = ['user', 'item', 'rating', 'review']
+csv_header = ['id', 'hip', 'hr', 'gl', 'lum','var','var_min','var_max']
+frame_header = ['id', 'hip', 'hr', 'gl', 'lum','var','var_min','var_max']
 
 # Con DictReader nosotros vamos a poder acceder a la cabecera, lo que se hará es recorrer nuestros campos, si hay un dato que falta
 #Insertaremos un dato de la cabecera para que así cada línea contenga el mismo número de columnas y se pueda leer el archivo
-with open('starts_data.csv', 'r') as f_input:
-    for row in csv.DictReader(f_input, delimiter=' ', fieldnames=csv_header[:-1], restkey=csv_header[-1], skipinitialspace=True):
+with open('starts_data_updated.csv', 'r') as f_input:
+    for row in csv.DictReader(f_input, delimiter='|', fieldnames=csv_header[:-1], restkey=csv_header[-1], quotechar='"', doublequote=True, skipinitialspace=True):
         try:
-            rows.append([row['user'], row['item'], row['rating'], ' '.join(row['review'])])
+            rows.append([row['id'], row['hip'], row['hr'], row['gl'], row['lum'], row['var'],row['var_min'], ' '.join(row['var_max'])])
         except KeyError as e:
-            rows.append([row['user'], row['item'], row['rating'], ' '])
+            rows.append([row['id'], row['hip'], row['hr'], row['gl'],row['lum'],row['var'], row['var_min'], ' '])
 frame = pd.DataFrame(rows, columns=frame_header)
 print (frame)
+#Convertimos nuestro DataFrame en CSV
+frame.to_csv('starts_data_encoding.csv', encoding='utf-8',quotechar='"',doublequote=True, sep = '|')
 
+df = pd.read_csv ('starts_data_encoding.csv')
 
-#Nuestro data Frame lo vamos a convertir en un CSV para que pueda ser leido con un encoding de utf-8
-frame.to_csv('outpout.csv', encoding='utf-8') 
+#Nos aseguramos que podemos leer nuestro archivo
+with open('starts_data_encoding.csv', 'rt') as f:
+    # same as csv_reader = csv.reader(f, skipinitialspace=True)
+    csv_reader = csv.reader(f, skipinitialspace=True,quotechar='"',doublequote=True)
+    
+    for line in csv_reader:
+        print(line)
 
-# Crearemos el archivo psv con las comillas faltantes
+    
 #Para el delimitador utilizamos funciones que contiene la librería de csv
-with open('starts_data.csv', 'r') as infile, open('output.psv', 'w') as outfile:
+with open('starts_data_encoding.csv', 'r') as infile, open('starts_data_with_quote.psv', 'w',encoding='utf-8') as outfile:
     reader = csv.reader(infile)
-    writer = csv.writer(outfile, delimiter='|')
+    writer = csv.writer(outfile,quoting=csv.QUOTE_ALL,escapechar='|')
     writer.writerows(reader)
